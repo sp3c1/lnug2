1cc46e28a7ff1701d7855f645dd6e8bb5b80e604
Bartlomiej Specjalny <bspecjalny@gmail.com>
2018-07-24 17:01:00 +0100

    used rhea.message to consumer/publisher
    Signed-off-by: Bartlomiej Specjalny <bspecjalny@gmail.com>

--- src/activemq/publisher.ts
+++ src/activemq/publisher.ts
@@ -1,4 +1,5 @@
 import { Amqp100Implementations } from './base';
+import * as Rhea from 'rhea/typings';
 
 export class PublisherAMQ100 extends Amqp100Implementations {
   constructor(public config: any) {
@@ -8,6 +9,6 @@ export class PublisherAMQ100 extends Amqp100Implementations {
   async publish(message: string | Buffer, queue: string = 'default', options: any = {}): Promise<any> {
     await this.createQueue(queue);
 
-    return this.handler.send({ body: message, application_properties: options });
+    return this.handler.send(<Rhea.Message>{ body: message, application_properties: options });
   }
 }
